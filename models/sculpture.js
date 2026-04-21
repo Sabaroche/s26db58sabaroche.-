9a3b1cd2b7c496415f5405fc24bdc08359a89f23
Sabaroche <S567597@nwmissouri.edu>
2026-04-21 02:45:51 -0500

    added constraints
    added a min and a max to the year attribute

--- models/sculpture.js
+++ models/sculpture.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const sculptureSchema = new mongoose.Schema({
     material: String,
     style: String,
-    year: Number
+    year: { type: Number, min: 1, max: 2026 }
 });
 
 module.exports = mongoose.model('Sculpture', sculptureSchema);
