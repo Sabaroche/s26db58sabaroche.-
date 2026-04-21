7828460f3b6bf78fd03f3311e86cc5f0c72262df
Sabaroche <S567597@nwmissouri.edu>
2026-04-21 02:32:19 -0500

    secured update page

--- routes/sculptures.js
+++ routes/sculptures.js
@@ -2,11 +2,16 @@ var express = require('express');
 var sculpture_controller = require('../controllers/sculpture');
 var router = express.Router();
 
-/* GET sculptures */
+function secured(req, res, next) {
+    if (req.user) { return next(); }
+    res.redirect('/login');
+}
+
 router.get('/', sculpture_controller.sculpture_view_all_Page);
 router.get('/detail', sculpture_controller.sculpture_view_one_page);
 router.get('/create', sculpture_controller.sculpture_create_Page);
-router.get('/update', sculpture_controller.sculpture_update_Page);
+router.get('/update', secured, sculpture_controller.sculpture_update_Page);
 router.get('/delete', sculpture_controller.sculpture_delete_Page);
+
 module.exports = router;
 
